f23b0c0e4d5bbdc4f8d02c32f53b7aa465026d91
sewon123 <sewon1630@gmail.com>
2019-05-29 21:13:11 +0900

    5.29 마지막

--- boot-thymeleaf2/src/main/java/idu/cs/domain/User.java
+++ boot-thymeleaf2/src/main/java/idu/cs/domain/User.java
@@ -13,9 +13,21 @@ public class User {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id; 
 	// database에서 sequence number, primary key 역할
-	
+	private String userId;
+	private String userPw;
 	private String name;
 	private String company;
+	
+	public User(String userId, String userPw, String name, String company) {
+		super();
+		this.userId = userId;
+		this.userPw = userPw;
+		this.name = name;
+		this.company = company;
+	}
+	
+	public User() {}
+	
 	
 	public Long getId() {
 		return id;
@@ -23,6 +35,20 @@ public class User {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -35,7 +61,7 @@ public class User {
 	public void setCompany(String company) {
 		this.company = company;
 	}
-	
+
 	
 	
 }
