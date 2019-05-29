d3bc0754b7a2913ab19260ef3d7c8e979a443f75
sizz103 <sizz103@nate.com>
2019-05-29 15:15:00 +0900

    5.29

---
+++ boot-thymeleaf2/src/main/java/idu/csa/maven/App.java
@@ -0,0 +1,13 @@
+package idu.csa.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
